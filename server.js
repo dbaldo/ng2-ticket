b6cce4073eed3e0d5bdbe9d0ccadc0a72c000acc
denis.baldo <denis.baldo@alertbrasil.com.br>
2018-08-30 15:09:55 -0300

    Ajustes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // server.js
 const express = require('express');
+const path = require('path');
 const app = express();
 const forceSSL = function() {
     return function (req, res, next) {
@@ -11,13 +12,23 @@ const forceSSL = function() {
       next();
     }
 }
+
 // Instruct the app
 // to use the forceSSL
 // middleware
 app.use(forceSSL());
+
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 80);
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
